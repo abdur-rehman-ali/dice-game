c19cab77a05aa96f62abe1ec9e32b23bbb600ef6
abdur-rehman-ali <alijoyia911@gmail.com>
2023-08-29 21:02:36 +0500

    Code refactoring

--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -1,35 +1,21 @@
 import { useState } from "react";
 
 const Play = () => {
-  const [activeOne, setActiveOne] = useState(false);
-  const [activeTwo, setActiveTwo] = useState(false);
-  const [activeThree, setActiveThree] = useState(false);
-  const [activeFour, setActiveFour] = useState(false);
-  const [activeFive, setActiveFive] = useState(false);
-  const [activeSix, setActiveSix] = useState(false);
+  const [activeButtons, setActiveButtons] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
-  const toggleActiveOne = () => {
-    setActiveOne(!activeOne);
-  };
-
-  const toggleActiveTwo = () => {
-    setActiveTwo(!activeTwo);
-  };
-
-  const toggleActiveThree = () => {
-    setActiveThree(!activeThree);
-  };
-
-  const toggleActiveFour = () => {
-    setActiveFour(!activeFour);
-  };
-
-  const toggleActiveFive = () => {
-    setActiveFive(!activeFive);
-  };
-
-  const toggleActiveSix = () => {
-    setActiveSix(!activeSix);
+  const toggleActiveButtons = (index) => {
+    setActiveButtons((previousActiveButtons) => {
+      const updatedProviousActiveButtons = [...previousActiveButtons];
+      updatedProviousActiveButtons[index] = !previousActiveButtons[index];
+      return updatedProviousActiveButtons;
+    });
   };
 
   return (
@@ -40,54 +26,17 @@ const Play = () => {
       </div>
       <div className="flex flex-col items-end justify-between">
         <div className="mb-2">
-          <button
-            className={`primary-button ${
-              activeOne ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveOne}
-          >
-            1
-          </button>
-          <button
-            className={`primary-button ${
-              activeTwo ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveTwo}
-          >
-            2
-          </button>
-          <button
-            className={`primary-button ${
-              activeThree ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveThree}
-          >
-            3
-          </button>
-          <button
-            className={`primary-button ${
-              activeFour ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveFour}
-          >
-            4
-          </button>
-          <button
-            className={`primary-button ${
-              activeFive ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveFive}
-          >
-            5
-          </button>
-          <button
-            className={`primary-button ${
-              activeSix ? "primary-button-active" : ""
-            }`}
-            onClick={toggleActiveSix}
-          >
-            6
-          </button>
+          {activeButtons.map((isActiveButton, index) => (
+            <button
+              className={`primary-button ${
+                isActiveButton ? "primary-button-active" : ""
+              }`}
+              key={index}
+              onClick={() => toggleActiveButtons(index)}
+            >
+              {index + 1}
+            </button>
+          ))}
         </div>
         <div className="mx-2">
           <h1 className="font-medium text-xl">Please Select a number</h1>
