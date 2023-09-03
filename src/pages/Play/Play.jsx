110fdaeed786fed5f472f7e471382493af2cc997
abdur-rehman-ali <alijoyia911@gmail.com>
2023-09-03 20:43:48 +0500

    Image refactoring

--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
-import dice1 from "../../assets/dice_1.png";
-import dice2 from "../../assets/dice_2.png";
-import dice3 from "../../assets/dice_3.png";
-import dice4 from "../../assets/dice_4.png";
-import dice5 from "../../assets/dice_5.png";
-import dice6 from "../../assets/dice_6.png";
-
-import Score from "./Score.jsx";
+import {
+  dice1,
+  dice2,
+  dice3,
+  dice4,
+  dice5,
+  dice6,
+} from "../../components/dice/dice";
+import Score from "./Score";
 import Menu from "./Menu";
 import Game from "./Game";
 
